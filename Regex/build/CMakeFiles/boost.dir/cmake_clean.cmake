5704dc1d628258f7194cb675d47892991347182f
Russell Best <ellrussbest@gmail.com>
2023-02-26 15:38:50 +0100

    first commit

---
+++ Regex/build/CMakeFiles/boost.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boost.dir/main.cpp.o"
+  "CMakeFiles/boost.dir/main.cpp.o.d"
+  "boost.html"
+  "boost.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boost.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Regex/main.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <boost/regex.hpp>
+#include <string>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::string s = "Boost Libraries";
+    boost::regex expr{"(\\w+)\\s(\\w+)"};
+    boost::smatch what;
+    if (boost::regex_search(s, what, expr))
+    {
+        std::cout << what[0] << '\n';
+        std::cout << what[1] << "_" << what[2] << '\n';
+    }
+
+    return 0;
+}
+
+// cd .. && rm -rf build && mkdir build && cd build
